c6f848b602c219818b9e97ab129e34ea7ac87c23
simptom569 <blednovegas@gmail.com>
2025-03-17 00:55:06 +0300

    edit file architecture

---
+++ src/api/dependencies.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+ 
+from fastapi import Depends, Query
+from pydantic import BaseModel
+
+
+class PaginationParams(BaseModel):
+    page_size: Annotated[int | None, Query(2, ge=1, lt=30, description="Кол-во элементов на странице")]
+    page_number: Annotated[int | None, Query(1, ge=1, description="Номер страницы")]
+
+
+PaginationDep = Annotated[PaginationParams, Depends()]
