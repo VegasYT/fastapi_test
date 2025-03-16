0d19b14af8e479b88514e5304e6e42d168c31ca9
simptom569 <blednovegas@gmail.com>
2025-03-16 19:10:51 +0300

    schemas and routers

--- main.py
+++ main.py
@@ -1,83 +1,11 @@
-from fastapi import FastAPI, Query, Body
+from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
-from typing import Optional
+from hotels import router as router_hotels
 
 
 app = FastAPI()
 
-# PUT
-class HotelUpdate(BaseModel):
-    id: int
-    title: str
-    name: str
-
-# PATCH
-class HotelPartialUpdate(BaseModel):
-    id: int
-    title: Optional[str] = None
-    name: Optional[str] = None
-
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "сочи"},
-    {"id": 2, "title": "Dubai", "name": "дубай"},
-]
-
-
-@app.get("/hotels")
-def get_hotels(
-        id: int | None = Query(None, description="Айдишник"),
-        title: str | None = Query(None, description="Название отеля"),
-):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
-    return hotels_
-
-
-@app.delete("/hotels/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
-    return {"status": "OK"}
-
-
-@app.post("/hotels")
-def cerate_hotel(
-    title: str = Body(embed=True),
-):
-    global hotels
-
-    hotels.append({
-        "id": hotels[-1]["id"] + 1, 
-        "title": title,
-        })
-    
-    return {"status": "ok"}
-
-
-@app.put("/hotels")
-def edit_hotel(hotel_data: HotelUpdate):
-    for hotel in hotels:
-        if hotel["id"] == hotel_data.id:
-            hotel["title"] = hotel_data.title
-            hotel["name"] = hotel_data.name
-
-    return {"status": "OK"}
-    
-
-@app.patch("/hotels")
-def partial_edit_hotel(hotel_data: HotelPartialUpdate):
-    for hotel in hotels:
-        if hotel["id"] == hotel_data.id:
-            update_data = hotel_data.dict(exclude_unset=True)
-            hotel.update(update_data)
-
-    return {"status": "OK"}
+app.include_router(router_hotels)
 
 
 if __name__ == "__main__":
