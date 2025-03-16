07260a23355e5b951eb21fed95ac62dc71a3b040
simptom569 <blednovegas@gmail.com>
2025-03-17 01:15:53 +0300

    add config.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.config import settings
 
+# print(settings.DB_NAME)
 
 app = FastAPI()
 
