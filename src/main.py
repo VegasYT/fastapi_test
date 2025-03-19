55e5fa99a9db1061a8ca3d91240e9ca228c9e71e
simptom569 <blednovegas@gmail.com>
2025-03-19 18:32:58 +0300

    db

--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
 from src.config import settings
+from src.database import *
+# print(settings.DB_URL)
 
-# print(settings.DB_NAME)
 
 app = FastAPI()
 
