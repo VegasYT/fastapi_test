bef24914b80e2832abd5a6fd3015840e206f9fcc
simptom569 <blednovegas@gmail.com>
2025-03-26 23:01:00 +0300

    validation for edit and delete

--- src/repos/base.py
+++ src/repos/base.py
@@ -1,5 +1,6 @@
+from fastapi import HTTPException
 from pydantic import BaseModel
-from sqlalchemy import delete, insert, select, update
+from sqlalchemy import delete, func, insert, select, update
 
 
 class BaseRepository:
@@ -8,18 +9,36 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
+    async def _validate_single_object(self, **filters_by) -> None:
+        count = await self.session.scalar(
+            select(func.count())
+            .select_from(self.model)
+            .filter_by(**filters_by)
+        )
+
+        if count == 0:
+            raise HTTPException(
+                status_code=404,
+                detail=f"{self.model.__name__} not found"
+            )
+        if count > 1:
+            raise HTTPException(
+                status_code=422,
+                detail=f"Несколько объектов {self.model.__name__} соответствуют критериям"
+            )
+
     async def get_all(self, *args, **kwargs):
         query = select(self.model)
         result = await self.session.execute(query)
 
-        return result.scalars().all() 
+        return result.scalars().all()
 
     async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
 
         return result.scalars().one_or_none()
-    
+
     async def add(self, add_data: BaseModel):
         add_stmt = (
             insert(self.model)
@@ -29,8 +48,10 @@ class BaseRepository:
 
         result = await self.session.execute(add_stmt)
         return result.scalar_one()
-    
+
     async def edit(self, update_data: BaseModel, **filters_by) -> None:
+        await self._validate_single_object(**filters_by)
+
         edit_stmt = (
             update(self.model)
             .filter_by(**filters_by)
@@ -40,11 +61,11 @@ class BaseRepository:
         await self.session.execute(edit_stmt)
 
     async def delete(self, **filters_by) -> None:
+        await self._validate_single_object(**filters_by)
+
         delete_stmt = (
             delete(self.model)
             .filter_by(**filters_by)
         )
 
         await self.session.execute(delete_stmt)
-
-    
