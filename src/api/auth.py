a7abeb1c7eef3cb2131d695d65ad5ec0ae5770ba
VegasYT <blednovegas@gmail.com>
2025-04-13 12:48:26 +0300

    reg exception

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,37 +1,40 @@
 from fastapi import APIRouter, HTTPException
 from passlib.context import CryptContext
- 
+from sqlalchemy.exc import IntegrityError
+
 from src.repos.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
 from src.database import async_session_maker
 
+
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
-
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.post("/register")
 async def register_user(
     data: UserRequestAdd
 ):
+    hashed_password = pwd_context.hash(data.password)
+    new_user_data = UserAdd(
+        email=data.email, 
+        hashed_password=hashed_password, 
+        first_name=data.first_name, 
+        last_name=data.last_name
+    )
+    
     async with async_session_maker() as session:
-        existing_user = await UsersRepository(session).get_one_or_none(email=data.email)
-        if existing_user:
-            raise HTTPException(
-                status_code=400,
-                detail="Пользователь с таким email уже существует"
-            )
-
-        hashed_password = pwd_context.hash(data.password)
-        new_user_data = UserAdd(
-            email=data.email, 
-            hashed_password=hashed_password, 
-            first_name=data.first_name, 
-            last_name=data.last_name
-        )
-
-        user = await UsersRepository(session).add(new_user_data)
-        await session.commit()
+        try:
+            user = await UsersRepository(session).add(new_user_data)
+            await session.commit()
+        except IntegrityError as e:
+            await session.rollback()
+            if "users_email_key" in str(e):
+                raise HTTPException(
+                    status_code=400,
+                    detail="Пользователь с таким email уже существует"
+                )
+            raise  # повторно вызываем исключение, если это не ошибка уникальности email
 
     return {"status": "OK"}
