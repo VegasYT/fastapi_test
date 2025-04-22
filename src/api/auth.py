7e709ba1084eb1428f85dd8ae489bb25471bf14b
VegasYT <blednovegas@gmail.com>
2025-04-22 14:17:36 +0300

    logout user

--- src/api/auth.py
+++ src/api/auth.py
@@ -63,3 +63,12 @@ async def get_me(
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_one_or_none(id=user_id)
         return user
+
+
+@router.post("/logout")
+async def logout_user(
+    response: Response
+):
+    response.delete_cookie("access_token")
+    
+    return {"status": "ok"}
