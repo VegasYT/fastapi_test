c3a71a530b8f7614330805a13448fe55780916bb
VEGAS <51318919+VegasYT@users.noreply.github.com>
2025-07-30 19:35:52 +0300

    db manager for users endpoints

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response
  
-from src.api.dependencies import UserIdDep
+from src.api.dependencies import DBDep, UserIdDep
 from src.repos.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd, UserRequestLogin
 from src.database import async_session_maker
@@ -12,6 +12,7 @@ router = APIRouter(prefix="/auth", tags=["Авторизация и аутент
 
 @router.post("/register")
 async def register_user(
+    db: DBDep,
     data: UserRequestAdd
 ):
     hashed_password = AuthService().pwd_context.hash(data.password)
@@ -22,51 +23,50 @@ async def register_user(
         last_name=data.last_name
     )
 
-    async with async_session_maker() as session:
-        existing_user = await UsersRepository(session).get_one_or_none(email=data.email)
-        if existing_user:
-            raise HTTPException(
-                status_code=400,
-                detail="Пользователь с таким email уже существует"
-            )
-        
-        user = await UsersRepository(session).add(new_user_data)
-        await session.commit()
+    existing_user = await db.users.get_one_or_none(email=data.email)
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Пользователь с таким email уже существует"
+        )
+    
+    user = await db.users.add(new_user_data)
 
     return {"status": "OK"}
 
 
 @router.post("/login")
 async def login_user(
+    db: DBDep,
     data: UserRequestLogin,
     response: Response
 ):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+    user = await db.users.get_user_with_hashed_password(email=data.email)
 
-        if not user:
-            raise HTTPException(status_code=401, detail="Неверный логин или пароль")
-        
-        if not AuthService().verify_password(data.password, user.hashed_password):
-            raise HTTPException(status_code=401, detail="Неверный логин или пароль")
-        
-        access_token = AuthService().create_access_token({"user_id": user.id})
-        response.set_cookie("access_token", access_token)
+    if not user:
+        raise HTTPException(status_code=401, detail="Неверный логин или пароль")
+    
+    if not AuthService().verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Неверный логин или пароль")
+    
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie("access_token", access_token)
 
-        return {"access_token": access_token}
+    return {"access_token": access_token}
     
 
 @router.get("/me")
 async def get_me(
+    db: DBDep,
     user_id: UserIdDep
 ):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(id=user_id)
-        return user
+    user = await db.users.get_one_or_none(id=user_id)
+    return user
 
 
 @router.post("/logout")
 async def logout_user(
+    db: DBDep,
     response: Response
 ):
     response.delete_cookie("access_token")
