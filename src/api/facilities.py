421fc37f86101b4bc48b982f663eba95ac288444
VegasYT <blednovegas@gmail.com>
2025-09-15 14:50:42 +0300

    upd cache router

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,36 +1,26 @@
-import json
-
 from fastapi import Body, APIRouter, Body
+from fastapi_cache.decorator import cache
 
 from src.schemas.facilities import FacilityAdd
 from src.api.dependencies import DBDep, PaginationDep
-from src.init import redis_manager
  
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
  
 
 @router.get("")
+@cache(expire=10)
 async def get_facilities(
     db: DBDep,
     pagination: PaginationDep,
 ):
-    # TODO: Учесть пагинацию в кешировании
     page_size = pagination.page_size or 5
     offset = page_size * (pagination.page_number - 1)
 
-    facilities_from_cache = await redis_manager.get("facilities")
-    if not facilities_from_cache:
-        print("ИДУ В БАЗУ ДАННЫХ")
-        facilities = await db.facilities.get_filtered_by_pagination(limit=page_size, offset=offset,)
-        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
-        facilities_json = json.dumps(facilities_schemas)
-        await redis_manager.set("facilities", facilities_json, 10)
-
-        return facilities
-    else:
-        facilities_dicts = json.loads(facilities_from_cache)
-        return facilities_dicts
+    return await db.facilities.get_filtered_by_pagination(
+        limit=page_size, 
+        offset=offset,
+    )
 
 
 @router.post("")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
-
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.auth import router as router_auth
@@ -20,6 +22,7 @@ from src.init import redis_manager
 async def lifespan(app: FastAPI):
     # При старте приложения
     await redis_manager.connect()
+    FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
     yield
     await redis_manager.close()
     # При выключении/перезагрузке приложения
